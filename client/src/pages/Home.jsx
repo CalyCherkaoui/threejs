2c7909120df8b1ad60fccffdabba1b91f76a2b44
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-11 15:40:54 +0000

    Implement Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,13 +17,27 @@ function Home() {
       {snap.intro && (
         <motion.section className='home' {...slideAnimation('left')}>
           {/* first thing to see on the screen */}
-          <motion.header>
+          <motion.header {...slideAnimation('down')}>
             <img
               src="./threejs.png"
               alt="logo"
               className='w-8 h-8 object-contain'
             />
           </motion.header>
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                OFFICIAL <br className='xl:block hidden'/> BOUTIQUE.
+              </h1>
+            </motion.div>
+            <motion.div className="flex flex-col gap-5" {...headContentAnimation}>
+              <p className='max-w-md font-normal text-gray-600 text-base'>
+                <strong>Unleash your creativity</strong>
+                and design your own unique and awesome shirt with our brand-new 3D tool
+              </p>
+              
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
