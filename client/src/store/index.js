d2d90752a40b0af4ae7490a4f2a096e01a401b35
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-11 14:34:15 +0000

    add state store with valtio

---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+// same as react context, it allow to define some flags
+import {proxy} from 'valtio'
+
+const state = proxy({
+  intro: true, //are we currentely on the home page
+  color: '#EFBD48', //default color
+  isLogoTexture: true, //are we displaying the logo on our shirt
+  isFullTexture: false,
+  logoDecal: './threejs.png', //empty default values
+  fullDecal: './threejs.png'//empty default values
+})
+
+export default state;
