cce75fd826dc246df03a61ac3405b62e1035c149
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-12 14:14:07 +0000

    implement colorPicker component

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { SketchPiker } from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 function ColorPicker() {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div
+      className='absolute left-full ml-3'
+    >
+      <SketchPiker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => state.color = color.hex}
+        presetColors={[ '#336699', '#86BBD8', 'e76f51', '023047', 'fb8500']}
+      />
+
+    </div>
   )
 }
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -12,6 +12,30 @@ import { AIPicker, ColorPicker, FilePicker, CustomButton, Tab } from '../compone
 
 function Customizer() {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+
+  // show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />
+      case 'aipicker':
+        return <AIPicker />
+      case 'filepicker':
+        return <FilePicker />
+      default:
+        return null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -27,9 +51,10 @@ function Customizer() {
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick={() =>{}}
+                    handleClick={() =>setActiveEditorTab(tab.name)}
                     />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
