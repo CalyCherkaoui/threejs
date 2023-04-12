b06bc4328f7ef7c0ee1dc607ad016db4af962502
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-12 11:24:27 +0000

    implement 3d animated backdrop

--- client/src/canvas/BackDrop.jsx
+++ client/src/canvas/BackDrop.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 function BackDrop() {
+  const shadows = useRef();
+
   return (
-    <div>BackDrop</div>
+    <AccumulativeShadows
+      ref={shadows}
+      position={[0, 0, -0.14]}
+      rotation={[Math.PI / 2, 0, 0]}
+      scale={10}
+      frames={60}
+      alphaTest={0.85}
+      temporal
+    >
+      <RandomizedLight
+        amount={4}
+      />
+    </AccumulativeShadows>
   )
 }
 
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -4,17 +4,21 @@ import { Environment, Center } from '@react-three/drei'
 import Shirt from './Shirt'
 import BackDrop from './BackDrop'
 import CameraRig from './CameraRig'
-import { AmbientLight } from 'three'
 
 
 function CanvasModel() {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className='w-full h-full max-w-full transition-all ease-in'
+    >
       <ambientLight intensity={0.5} />
       <Environment preset='city' />
 
       <CameraRig>
-        {/* <BackDrop /> */}
+        <BackDrop />
         <Center>
           <Shirt />
         </Center>
