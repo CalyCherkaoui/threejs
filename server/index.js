71a641132f48da048b0f77e25c896a3bc0b56912
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-13 14:57:49 +0000

    implement route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import dalleRoutes from './routes/dalle.routes';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use('/api/v1/dalle' , dalleRoutes);
 
 app.get('/', (req, res) => {
   // res.status(200).send('Hello this is DALL.E!');
