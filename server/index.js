48d609909118b26ba755b5546c62ad939298d31e
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2023-04-13 14:42:32 +0000

    build the server

--- server/index.js
+++ server/index.js
@@ -9,5 +9,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello this is DALL.E!');
+  // res.status(200).send('Hello this is DALL.E!');
+  res.status(200).json({ message: 'Hello this is DALL.E!'});
 });
+
+app.listen(8080, () => {
+  console.log(`Server is running on porti ${8080}`);
+} );
